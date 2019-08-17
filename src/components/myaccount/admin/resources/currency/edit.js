8e5f4d22098a307f46d47715e36837901b89bedf
ameerhamza0403 <ah04031996@gmail.com>
2019-08-17 13:11:28 +0500

    update

--- src/components/myaccount/admin/resources/currency/edit.js
+++ src/components/myaccount/admin/resources/currency/edit.js
@@ -1,72 +1,115 @@
-import React, { Component, useEffect } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
-import Modal from "@material-ui/core/Modal";
-import TextField from "@material-ui/core/TextField";
-import clsx from "clsx";
+import React, { Component, useEffect, useState } from "react";
 import { GetcurrencyDataById, PutcurrencyDataById } from "..//shared/currency";
+import {
+  Button,
+  Card,
+  CardBody,
+  Col,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+  Row,
+  Form,
+  FormFeedback,
+  FormGroup,
+  Label,
+  Input
+} from "reactstrap";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
-function rand() {
-  return Math.round(Math.random() * 20) - 10;
-}
-
-function getModalStyle() {
-  const top = 50 + rand();
-  const left = 50 + rand();
-
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`
-  };
-}
-
-const useStyles = makeStyles(theme => ({
+const classes = {
   button: {
-    margin: theme.spacing(1),
-    backgroundColor: "#F4662F",
-    color: "white"
+    color: "white",
+    backgroundColor: "#EE7647",
+    border: "none"
   },
-  input: {
-    display: "none"
-  },
-  paper: {
-    position: "absolute",
-    width: 400,
-    backgroundColor: theme.palette.background.paper,
-    border: "2px solid #F4662F",
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 4),
-    outline: "none"
-  },
-  textField: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1)
-  },
-  dense: {
-    marginTop: theme.spacing(2)
+  plusbutton: {
+    color: "white",
+    borderRadius: "50px",
+    width: "10px",
+    cursor: "pointer",
+    float: "left"
+    // marginTop: '10px',
+    // marginLeft: '5px',
   }
-}));
+};
 
 let EditCurrency = props => {
-  const classes = useStyles();
   // getModalStyle is not a pure function, we roll the style only on the first render
-  const [modalStyle] = React.useState(getModalStyle);
-  // const [open, setOpen] = React.useState(false);
+  async function onSubmit(values, { setSubmitting, setErrors }) {
+    await PutcurrencyDataById(props.IDforAPI, values);
+    handleOpen();
+    props.refresh();
+    setSubmitting(false);
+  }
 
-  // const handleOpen = () => {
-  // setOpen(true);
-  // };
-
-  const handleClose = () => {
-    // setOpen(false);
-    props.cross();
+  const validationSchema = function(values) {
+    return Yup.object().shape({
+      name: Yup.string()
+        .min(4, `Currency Name has to be at least 4 characters`)
+        .required("Currency Name is required"),
+      code: Yup.string()
+        .min(3, `Currency Code has to be at least 3 characters`)
+        .required("Currency Code is required")
+    });
   };
 
-  const [editvalues, seteditValues] = React.useState({});
+  const validate = getValidationSchema => {
+    return values => {
+      const validationSchema = getValidationSchema(values);
+      try {
+        validationSchema.validateSync(values, { abortEarly: false });
+        return {};
+      } catch (error) {
+        return getErrorsFromValidationError(error);
+      }
+    };
+  };
 
-  const handleEditChange = name => event => {
-    seteditValues({ ...editvalues, [name]: event.target.value });
+  const getErrorsFromValidationError = validationError => {
+    const FIRST_ERROR = 0;
+    return validationError.inner.reduce((errors, error) => {
+      return {
+        ...errors,
+        [error.path]: error.errors[FIRST_ERROR]
+      };
+    }, {});
+  };
+
+  const [initialValues, setInitialValues] = useState({
+    name: "",
+    code: "",
+    active: false
+  });
+
+  function findFirstError(formName, hasError) {
+    const form = document.forms[formName];
+    for (let i = 0; i < form.length; i++) {
+      if (hasError(form[i].name)) {
+        form[i].focus();
+        break;
+      }
+    }
+  }
+
+  function validateForm(errors) {
+    findFirstError("simpleForm", fieldName => {
+      return Boolean(errors[fieldName]);
+    });
+  }
+
+  function touchAll(setTouched, errors) {
+    setTouched({
+      name: true
+    });
+    validateForm(errors);
+  }
+  let [modal, setModal] = useState(true);
+
+  let handleOpen = () => {
+    return setModal((modal = false)), setTimeout(() => props.cross(), 200);
   };
 
   useEffect(() => {
@@ -74,71 +117,134 @@ let EditCurrency = props => {
   }, []);
 
   async function getlistapi() {
-    const { data: editvalues } = await GetcurrencyDataById(props.IDforAPI);
-    seteditValues(editvalues);
-  }
-
-  async function putlistapi() {
-    await PutcurrencyDataById(props.IDforAPI, editvalues);
-    handleClose();
-    props.refresh();
+    const { data: initialValues } = await GetcurrencyDataById(props.IDforAPI);
+    setInitialValues(initialValues);
   }
 
   return (
     <div>
       <Modal
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
-        open={props.click}
-        onClose={handleClose}
+        isOpen={modal}
+        toggle={handleOpen}
+        className={"modal-primary " + props.className}
       >
-        <div style={modalStyle} className={classes.paper}>
+        <ModalHeader toggle={handleOpen}>Add Currency</ModalHeader>
+        <ModalBody>
           <div className="container">
-            <div className="row">
-              <form>
-                <div class="form-group">
-                <label for="exampleInputEmail1">Currency Name</label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                    placeholder={editvalues.name}
-                    onChange={handleEditChange("name")}
-                  />
-                  <label for="exampleInputEmail1">Code</label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                    placeholder={editvalues.code}
-                    onChange={handleEditChange("code")}
-                  />
-                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                  <input
-                    className="form-check-input"
-                    type="checkbox"
-                    value="true"
-                    id="defaultCheck2"
-                    onChange={handleEditChange("active")}
-                  />
-                  <label className="form-check-label" for="defaultCheck2">
-                    Active
-                  </label>
-                </div>
-              </form>
-            </div>
-          </div>
+            <Formik
+              initialValues={initialValues}
+              validate={validate(validationSchema)}
+              onSubmit={onSubmit}
+              render={({
+                values,
+                errors,
+                touched,
+                status,
+                dirty,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting,
+                isValid,
+                handleReset,
+                setTouched
+              }) => (
+                <Row>
+                  <Col lg="12">
+                    <Form onSubmit={handleSubmit} noValidate name="simpleForm">
+                      <FormGroup>
+                        <div className="row">
+                          <div className="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
+                            <Label for="name">Currency Name</Label>
+                          </div>
+                          <div className="col-12 col-sm-12 col-md-6 col-lg-8 col-xl-8">
+                            <Input
+                              type="text"
+                              name="name"
+                              id="name"
+                              placeholder={initialValues.name}
+                              autoComplete="given-name"
+                              valid={!errors.name}
+                              invalid={touched.name && !!errors.name}
+                              autoFocus={true}
+                              required
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                              value={values.name}
+                            />
+                            <FormFeedback>{errors.name}</FormFeedback>
+                            <br />
+                          </div>
+                        </div>
+                        <div className="row">
+                          <div className="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
+                            <Label for="name">Currency Code</Label>
+                          </div>
+                          <div className="col-12 col-sm-12 col-md-6 col-lg-8 col-xl-8">
+                            <Input
+                              type="text"
+                              name="code"
+                              id="code"
+                              placeholder={initialValues.code}
+                              autoComplete="given-name"
+                              valid={!errors.code}
+                              invalid={touched.code && !!errors.code}
+                              autoFocus={true}
+                              required
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                              value={values.code}
+                            />
+                            <FormFeedback>{errors.code}</FormFeedback>
+                            <br />
+                            <input
+                              name="active"
+                              id="active"
+                              valid={!errors.active}
+                              invalid={touched.active && !!errors.active}
+                              onClick={handleChange}
+                              onBlur={handleBlur}
+                              value={values.active}
+                              type="checkbox"
+                            />
+                            &nbsp;&nbsp;&nbsp;
+                            <label
+                              className="form-check-label"
+                              for="defaultCheck1"
+                            >
+                              Active
+                            </label>
+                          </div>
+                        </div>
+                      </FormGroup>
+                      <FormGroup>
+                        <ModalFooter>
+                          <Button
+                            type="submit"
+                            color="primary"
+                            className="mr-1"
+                            style={classes.button}
+                            disabled={isSubmitting || !isValid}
+                          >
+                            {isSubmitting ? "Wait..." : "Submit"}
+                          </Button>
 
-          <Button
-            variant="contained"
-            className={classes.button}
-            onClick={putlistapi}
-          >
-            Save
-          </Button>
-        </div>
+                          <Button
+                            color="secondary"
+                            onClick={handleOpen}
+                            style={classes.button}
+                          >
+                            Cancel
+                          </Button>
+                        </ModalFooter>
+                      </FormGroup>
+                    </Form>
+                  </Col>
+                </Row>
+              )}
+            />
+          </div>
+        </ModalBody>
       </Modal>
     </div>
   );
--- src/components/myaccount/admin/resources/group/listing.js
+++ src/components/myaccount/admin/resources/group/listing.js
@@ -1,25 +1,48 @@
 import React, { useState, useEffect } from "react";
 import MUIDataTable from "mui-datatables";
-import "./listing.css";
+import "./listing.scss";
 import EditGroup from "./edit";
 import {
   GetListingForResourceGroup,
   DeleteResourceGroupDataById
 } from "..//shared/resourcegroup";
-import ResourceAddGroup from './add';
-import LinearProgress from '@material-ui/core/LinearProgress';
-
+import ResourceAddGroup from "./add";
+import LinearProgress from "@material-ui/core/LinearProgress";
 
 let menuDiv = "";
 let EditshowModel = "";
 let idofEdit = 0;
-let count = false;
 
-
+const classes = {
+  linearprogress: {
+    // backgroundColor: '#EE7647',
+    backgroundColor: "rgb(243, 153, 117)"
+  },
+  header: {
+    backgroundColor: "#EE7647",
+    height: "40px",
+    borderRadius: "5px",
+    width: "99%",
+    marginLeft: "0.5%",
+    alignContent: "center",
+    color: "white"
+  },
+  heading: {
+    marginRight: "10%",
+    textAlign: "right"
+  },
+  plusign: {
+    marginLeft: "80%"
+  },
+  actions: {
+    cursor: "pointer",
+    float: "left",
+    marginLeft: "70%"
+  }
+};
 
 let ResourceGroupListing = () => {
   let [Atlist, setAtlist] = useState([]);
-
 
   const columns = [
     {
@@ -53,35 +76,36 @@ let ResourceGroupListing = () => {
   const options = {
     filterType: "multiselect",
     onRowClick: (rowData, rowMeta) => HandlerowSelect(rowData, rowMeta),
+    customToolbar: () => console.log("rowData"),
     rowsPerPageOptions: [2, 5, 10, 15, 20, 100],
     selectableRows: "none",
     viewColumns: true
+
     // onRowsSelect: (currentRowsSelected, allRowsSelected) => console.log(currentRowsSelected, ' : ', allRowsSelected ),
   };
 
-
-
-  let Tabledisplay=<LinearProgress />
-  let [Tabledistatus, settabledistatus]=useState(false)
-  if(Tabledistatus){
-    Tabledisplay=<MUIDataTable
-    title={"Resource Group"}
-    data={Atlist}
-    columns={columns}
-    options={options}
-
-/>;
+  let Tabledisplay = (
+    <LinearProgress style={classes.linearprogress} color="secondary" />
+  );
+  let [Tabledistatus, settabledistatus] = useState(false);
+  if (Tabledistatus) {
+    Tabledisplay = (
+      <MUIDataTable
+        title={"Actions & Filters"}
+        data={Atlist}
+        columns={columns}
+        options={options}
+      />
+    );
+  } else {
+    Tabledisplay = (
+      <LinearProgress style={classes.linearprogress} color="secondary" />
+    );
   }
-  else{
-    Tabledisplay=<LinearProgress />
-  }
-  let refreshfn=()=>{
-    settabledistatus(Tabledistatus=false)
+  let refreshfn = () => {
+    settabledistatus((Tabledistatus = false));
     getlistapi();
-  }
-
-
-
+  };
 
   useEffect(() => {
     getlistapi();
@@ -90,7 +114,11 @@ let ResourceGroupListing = () => {
   async function getlistapi() {
     const { data: Atlist } = await GetListingForResourceGroup();
     setAtlist(Atlist);
-    settabledistatus(Tabledistatus=true)
+    // Atlist.map((e,i)=>
+    //   Atlist[i].action=<i className="icon-options icons font-2xl d-block mt-4" ></i>
+
+    //                 )
+    settabledistatus((Tabledistatus = true));
   }
 
   async function Dellistapi() {
@@ -98,7 +126,6 @@ let ResourceGroupListing = () => {
     Handlerowclose();
     refreshfn();
   }
-
 
   let [Editstate, setEditstate] = React.useState(false);
   let HandleEditforlisting = () => {
@@ -116,10 +143,9 @@ let ResourceGroupListing = () => {
   if (Editstate) {
     EditshowModel = (
       <EditGroup
-        click={Editstate}
         IDforAPI={idofEdit}
-        cross={HandleCrossEditforlisting}
         refresh={refreshfn}
+        cross={HandleCrossEditforlisting}
       />
     );
   } else {
@@ -137,18 +163,17 @@ let ResourceGroupListing = () => {
   };
   if (menushow) {
     menuDiv = (
-      <div className="container menu">
-        <div className="row menu">
-         <button className='btn btn-outline-secondary btn-sm' onClick={HandleEditforlisting}>
-            Edit</button>&nbsp;&nbsp;
-            <button className='btn btn-outline-secondary btn-sm'
-            onClick={Dellistapi}
-          >
-            Delete</button>&nbsp;&nbsp;
-            <button className='btn btn-outline-secondary btn-sm'
-            onClick={Handlerowclose}
-          >
-            Close</button>
+      <div className="row" style={classes.actions}>
+        <div onClick={HandleEditforlisting}>
+          <i className="fa fa-pencil-square fa-2x" />
+        </div>
+        <div onClick={Dellistapi}>
+          &nbsp;&nbsp;
+          <i className="fa fa-archive fa-2x" />
+        </div>
+        <div onClick={Handlerowclose}>
+          &nbsp;&nbsp;
+          <i className="fa fa-times-rectangle fa-2x" />
         </div>
       </div>
     );
@@ -158,8 +183,22 @@ let ResourceGroupListing = () => {
 
   return (
     <div>
-      <ResourceAddGroup refresh={refreshfn}/>
-      {menuDiv}
+      <div className="row" style={classes.header}>
+        <div className="col-12 col-sm-6 col-md-7 col-lg-7 col-xl-7">
+          <h3 style={classes.heading}>RESOURCE GROUP</h3>
+        </div>
+        <div className="col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4">
+          {menuDiv}
+        </div>
+        <div className="col-12 col-sm-6 col-md-1 col-lg-1 col-xl-1">
+          <div className="row" style={classes.plusign}>
+            <ResourceAddGroup refresh={refreshfn} />
+          </div>
+        </div>
+      </div>
+      <br />
+      <br />
+      <br />
       {EditshowModel}
       {Tabledisplay}
     </div>
