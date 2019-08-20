b92e2dd569b48631ccc9f144b26946a532e47f7e
ameerhamza0403 <ah04031996@gmail.com>
2019-08-20 17:51:28 +0500

    20-08-19

--- src/components/myaccount/admin/resources/shared/driverbeh.js
+++ src/components/myaccount/admin/resources/shared/driverbeh.js
@@ -1,57 +1,110 @@
-import { apiUrl } from '..//..//..//..//..//config.json';
-import axios from 'axios';
-
-
+import { apiUrl } from "..//..//..//..//..//config.json";
+import axios from "axios";
 
 /* *************************DriverBeh Type*********************** */
- let apiurlDriverBeh= apiUrl + '/masterdata/DrvBehKPIs';
+let apiurlDriverBeh = apiUrl + "/masterdata/DrvBehKPIs";
+let apiurlDriverBehbyresource =
+  apiUrl + "/masterdata/DrvBehKPISettings/byresourcegroup";
+let apiurlDriverBehSet = apiUrl + "/masterdata/DrvBehKPISettings";
 // let apiurlDriverBeh= 'https://reqres.in/api/users/2';
 
 function ApiwithidDriverBeh(Id) {
-    return `${apiurlDriverBeh}/${Id}`;
-  }
+  return `${apiurlDriverBeh}/${Id}`;
+}
+
+function ApiwithidDriverBehbyResID(Id) {
+  return `${apiurlDriverBehbyresource}/${Id}`;
+}
+
+function ApiwithidDriverBehSet(Id) {
+  return `${apiurlDriverBehSet}/${Id}`;
+}
 
 //-------- GetAPi
 export function GetListingForDriverBeh() {
-    return axios.get(apiurlDriverBeh,
-             {headers: {
-                 //"Authorization": authIt,
-                 //"accept": "application/json",
-                 //"Access-Control-Allow-Origin": "*",
-                 'Content-Type': 'application/json',
-                }}
-            )
-
+  return axios.get(apiurlDriverBeh, {
+    headers: {
+      //"Authorization": authIt,
+      //"accept": "application/json",
+      //"Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json"
+    }
+  });
 }
 
 //---------Edit APi
 export function GetDriverBehDataById(id) {
-    // console.log(id)
-    return axios.get(ApiwithidDriverBeh(id),
-             {headers: {
-                 'Content-Type': 'application/json',
-                }}
-            )
-
+  // console.log(id)
+  return axios.get(ApiwithidDriverBeh(id), {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
 }
 
-export function PutDriverBehDataById(id,body) {
-    // console.log(id)
-    return axios.put(ApiwithidDriverBeh(id),body)
-
+export function PutDriverBehDataById(id, body) {
+  // console.log(id)
+  return axios.put(ApiwithidDriverBeh(id), body);
 }
 
 //------- Delete-API
 
 export function DeleteDriverBehDataById(id) {
-    // console.log(id)
-    return axios.delete(ApiwithidDriverBeh(id))
-
+  // console.log(id)
+  return axios.delete(ApiwithidDriverBeh(id));
 }
 
 //----------Save-Post-API
 export function PostListingForDriverBeh(body) {
-    return axios.post(apiurlDriverBeh,body)
-
+  return axios.post(apiurlDriverBeh, body);
 }
 
+//-------- Get By Resource ID-----
+
+export function GetDriverBehDataByResId(id) {
+  // console.log(id)
+  return axios.get(ApiwithidDriverBehbyResID(id), {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+}
+
+//-------- GetAPi
+export function GetListingForDriverBehSet() {
+  return axios.get(apiurlDriverBehSet, {
+    headers: {
+      //"Authorization": authIt,
+      //"accept": "application/json",
+      //"Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json"
+    }
+  });
+}
+
+//---------Edit APi
+export function GetDriverBehSetDataById(id) {
+  // console.log(id)
+  return axios.get(ApiwithidDriverBehSet(id), {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+}
+
+export function PutDriverBehDataSetById(id, body) {
+  // console.log(id)
+  return axios.put(ApiwithidDriverBehSet(id), body);
+}
+
+//------- Delete-API
+
+export function DeleteDriverBehSetDataById(id) {
+  // console.log(id)
+  return axios.delete(ApiwithidDriverBehSet(id));
+}
+
+//----------Save-Post-API
+export function PostListingForDriverBehSet(body) {
+  return axios.post(apiurlDriverBehSet, body);
+}
