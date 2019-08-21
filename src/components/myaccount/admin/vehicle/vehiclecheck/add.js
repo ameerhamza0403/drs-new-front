4ca98974ff0325af285c41792f0e1fa6a74a24e1
ameerhamza0403 <ah04031996@gmail.com>
2019-08-21 14:27:57 +0500

    2:27

---
+++ src/components/myaccount/admin/vehicle/vehiclecheck/add.js
@@ -0,0 +1,275 @@
+import React, { Component, useState, useEffect } from "react";
+import { PostListingForcurrency } from "..//shared/currency";
+import {
+  Button,
+  Card,
+  CardBody,
+  Col,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+  Row,
+  Form,
+  FormFeedback,
+  FormGroup,
+  Label,
+  Input
+} from "reactstrap";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const classes = {
+  button: {
+    color: "white",
+    backgroundColor: "#EE7647",
+    border: "none"
+  },
+  plusbutton: {
+    color: "white",
+    borderRadius: "50px",
+    width: "10px",
+    cursor: "pointer",
+    float: "left"
+    // marginTop: '10px',
+    // marginLeft: '5px',
+  }
+};
+
+let AddCurrency = props => {
+  // getModalStyle is not a pure function, we roll the style only on the first render
+
+  //Tost
+
+  function errort() {
+    // add type: 'error' to options
+    return toast.error("Failed with Error...", {
+      position: toast.POSITION.BOTTOM_RIGHT
+    });
+  }
+  function success(response) {
+    if (response == "Exception Error") {
+      return toast.error('Failed with Error "' + response + '"', {
+        position: toast.POSITION.BOTTOM_RIGHT
+      });
+    } else {
+      return toast.success(response, {
+        position: toast.POSITION.BOTTOM_RIGHT
+      });
+    }
+  }
+
+  async function onSubmit(values, { setSubmitting, setErrors }) {
+    await PostListingForcurrency(values)
+      .then(res => success(res.data.message))
+      .catch(error => errort());
+    handleOpen();
+    props.refresh();
+    setSubmitting(false);
+  }
+
+  const validationSchema = function(values) {
+    return Yup.object().shape({
+      name: Yup.string()
+        .min(4, `Currency name has to be at least 4 characters`)
+        .required("Name is required"),
+      code: Yup.string()
+        .max(3, `Currency Code has to be at least 3 characters`)
+        .required("Code is required")
+    });
+  };
+
+  const validate = getValidationSchema => {
+    return values => {
+      const validationSchema = getValidationSchema(values);
+      try {
+        validationSchema.validateSync(values, { abortEarly: false });
+        return {};
+      } catch (error) {
+        return getErrorsFromValidationError(error);
+      }
+    };
+  };
+
+  const getErrorsFromValidationError = validationError => {
+    const FIRST_ERROR = 0;
+    return validationError.inner.reduce((errors, error) => {
+      return {
+        ...errors,
+        [error.path]: error.errors[FIRST_ERROR]
+      };
+    }, {});
+  };
+
+  const initialValues = {
+    name: "",
+    code: "",
+    active: false
+  };
+
+  function findFirstError(formName, hasError) {
+    const form = document.forms[formName];
+    for (let i = 0; i < form.length; i++) {
+      if (hasError(form[i].name)) {
+        form[i].focus();
+        break;
+      }
+    }
+  }
+
+  function validateForm(errors) {
+    findFirstError("simpleForm", fieldName => {
+      return Boolean(errors[fieldName]);
+    });
+  }
+
+  function touchAll(setTouched, errors) {
+    setTouched({
+      name: true
+    });
+    validateForm(errors);
+  }
+
+  let [modal, setModal] = useState(false);
+
+  let handleOpen = () => {
+    return setModal((modal = !modal));
+  };
+
+  return (
+    <div>
+      <div onClick={handleOpen} style={classes.plusbutton}>
+        <i className="fa fa-plus-circle fa-2x" />
+      </div>
+
+      <Modal
+        isOpen={modal}
+        toggle={handleOpen}
+        className={"modal-primary " + props.className}
+      >
+        <ModalHeader toggle={handleOpen}>Add Currency</ModalHeader>
+        <ModalBody>
+          <div className="container">
+            <Formik
+              initialValues={initialValues}
+              validate={validate(validationSchema)}
+              onSubmit={onSubmit}
+              render={({
+                values,
+                errors,
+                touched,
+                status,
+                dirty,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting,
+                isValid,
+                handleReset,
+                setTouched
+              }) => (
+                <Row>
+                  <Col lg="12">
+                    <Form onSubmit={handleSubmit} noValidate name="simpleForm">
+                      <FormGroup>
+                        <div className="row">
+                          <div className="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
+                            <Label for="name">Currency Name</Label>
+                          </div>
+                          <div className="col-12 col-sm-12 col-md-6 col-lg-8 col-xl-8">
+                            <Input
+                              type="text"
+                              name="name"
+                              id="name"
+                              placeholder="i.e. United States Dollar"
+                              autoComplete="given-name"
+                              valid={!errors.name}
+                              invalid={touched.name && !!errors.name}
+                              autoFocus={true}
+                              required
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                              value={values.name}
+                            />
+                            <FormFeedback>{errors.name}</FormFeedback>
+                            <br />
+                          </div>
+                        </div>
+                        <div className="row">
+                          <div className="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
+                            <Label for="name">Currency Code</Label>
+                          </div>
+                          <div className="col-12 col-sm-12 col-md-6 col-lg-8 col-xl-8">
+                            <Input
+                              type="text"
+                              name="code"
+                              id="code"
+                              placeholder="i.e. USD"
+                              autoComplete="given-name"
+                              valid={!errors.code}
+                              invalid={touched.code && !!errors.code}
+                              autoFocus={true}
+                              required
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                              value={values.code}
+                              maxlength={3}
+                            />
+                            <FormFeedback>{errors.code}</FormFeedback>
+                            <br />
+                            <input
+                              name="active"
+                              id="active"
+                              valid={!errors.active}
+                              invalid={touched.active && !!errors.active}
+                              onClick={handleChange}
+                              onBlur={handleBlur}
+                              value={values.active}
+                              type="checkbox"
+                            />
+                            &nbsp;&nbsp;&nbsp;
+                            <label
+                              className="form-check-label"
+                              for="defaultCheck1"
+                            >
+                              Active
+                            </label>
+                          </div>
+                        </div>
+                      </FormGroup>
+                      <FormGroup>
+                        <ModalFooter>
+                          <Button
+                            type="submit"
+                            color="primary"
+                            className="mr-1"
+                            style={classes.button}
+                            disabled={isSubmitting || !isValid}
+                          >
+                            {isSubmitting ? "Wait..." : "Submit"}
+                          </Button>
+
+                          <Button
+                            color="secondary"
+                            onClick={handleOpen}
+                            style={classes.button}
+                          >
+                            Cancel
+                          </Button>
+                        </ModalFooter>
+                      </FormGroup>
+                    </Form>
+                  </Col>
+                </Row>
+              )}
+            />
+          </div>
+        </ModalBody>
+      </Modal>
+    </div>
+  );
+};
+
+export default AddCurrency;
