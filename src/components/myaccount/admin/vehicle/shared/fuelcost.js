6b549c2a8d3b39e6ba4c940d8f5d2c3d4370fc2d
ameerhamza0403 <ah04031996@gmail.com>
2019-08-22 16:52:14 +0500

    4:52

--- src/components/myaccount/admin/vehicle/shared/fuelcost.js
+++ src/components/myaccount/admin/vehicle/shared/fuelcost.js
@@ -1,56 +1,53 @@
-import { apiUrl } from '..//..//..//..//..//config.json';
-import axios from 'axios';
-
-
+import { apiUrl } from "..//..//..//..//..//config.json";
+import axios from "axios";
 
 /* *************************VVehicle Fuel Cost*********************** */
- let apiurlVehiclefuelcost= apiUrl + '/masterdata/Vehiclefuelcosts';
+let apiurlVehiclefuelcost = apiUrl + "/masterdata/FuelCost";
 // let apiurlVehiclefuelcost= 'https://reqres.in/api/users/2';
 
 function ApiwithidVehiclefuelcost(Id) {
-    return `${apiurlVehiclefuelcost}/${Id}`;
-  }
+  return `${apiurlVehiclefuelcost}/${Id}`;
+}
+
+function ApibyPageVehiclefuelcost(Pg, PgSize) {
+  return `${apiurlVehiclefuelcost}/${Pg}/${PgSize}`;
+}
 
 //-------- GetAPi
-export function GetListingForVehiclefuelcost() {
-    return axios.get(apiurlVehiclefuelcost,
-             {headers: {
-                 //"Authorization": authIt,
-                 //"accept": "application/json",
-                 //"Access-Control-Allow-Origin": "*",
-                 'Content-Type': 'application/json',
-                }}
-            )
-
+export function GetListingForVehiclefuelcost(pg, pgsize) {
+  return axios.get(ApibyPageVehiclefuelcost(pg, pgsize), {
+    headers: {
+      //"Authorization": authIt,
+      //"accept": "application/json",
+      //"Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json"
+    }
+  });
 }
 
 //---------Edit APi
 export function GetVehiclefuelcostDataById(id) {
-    // console.log(id)
-    return axios.get(ApiwithidVehiclefuelcost(id),
-             {headers: {
-                 'Content-Type': 'application/json',
-                }}
-            )
-
+  // console.log(id)
+  return axios.get(ApiwithidVehiclefuelcost(id), {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
 }
 
-export function PutVehiclefuelcostDataById(id,body) {
-    // console.log(id)
-    return axios.put(ApiwithidVehiclefuelcost(id),body)
-
+export function PutVehiclefuelcostDataById(id, body) {
+  // console.log(id)
+  return axios.put(ApiwithidVehiclefuelcost(id), body);
 }
 
 //------- Delete-API
 
 export function DeleteVehiclefuelcostDataById(id) {
-    // console.log(id)
-    return axios.delete(ApiwithidVehiclefuelcost(id))
-
+  // console.log(id)
+  return axios.delete(ApiwithidVehiclefuelcost(id));
 }
 
 //----------Save-Post-API
 export function PostListingForVehiclefuelcost(body) {
-    return axios.post(apiurlVehiclefuelcost,body)
-
+  return axios.post(apiurlVehiclefuelcost, body);
 }
