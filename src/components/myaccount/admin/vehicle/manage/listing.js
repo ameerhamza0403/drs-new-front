b72c2b28187031abd883050ae0dca2300404bfe7
ameerhamza0403 <ah04031996@gmail.com>
2019-08-24 00:25:29 +0500

    hamza

--- src/components/myaccount/admin/vehicle/manage/listing.js
+++ src/components/myaccount/admin/vehicle/manage/listing.js
@@ -1,8 +1,626 @@
-import React, {Component, useState} from 'react';
+import React, { useState, useEffect } from "react";
+// import MUIDataTable from "mui-datatables";
+import EditFuelCost from "./edit";
+import VehicleFuelCostAdd from "./add";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "../../../../../scss/override/listing.scss";
+import {
+  GetListingForVehiclemanage,
+  DeleteVehiclemanageDataById
+} from "../shared/manage";
+import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
+import "react-bootstrap-table/dist//react-bootstrap-table-all.min.css";
+import { Pagination, PaginationItem, PaginationLink } from "reactstrap";
+import DoneOutlineIcon from '@material-ui/icons/DoneOutline';
+import Icon2 from '@material-ui/icons/Clear';
 
+let menuDiv = "";
+let EditshowModel = "";
+let idofEdit = 0;
+let Page = 1;
+let PageSize = 10;
+let paging = "";
+let TotalPages = 3;
 
+const classes = {
+  linearprogress: {
+    // backgroundColor: '#EE7647',
+    backgroundColor: "rgb(243, 153, 117)"
+  },
+  header: {
+    backgroundColor: "#EE7647",
+    height: "40px",
+    borderRadius: "5px",
+    width: "99%",
+    marginLeft: "0.5%",
+    alignContent: "center",
+    color: "white"
+  },
+  heading: {
+    marginRight: "10%",
+    textAlign: "right"
+  },
+  plusign: {
+    marginLeft: "80%"
+  },
+  actions: {
+    cursor: "pointer",
+    float: "left",
+    marginLeft: "70%"
+  }
+};
 let VehicleManage =()=>{
-  return null;
+
+  let [Atlist, setAtlist] = useState([]);
+  let [paginate, setPaginate] = useState();
+
+  //-- React Data Table
+  const options = {
+    sortIndicator: true,
+    // page: Page,
+    hideSizePerPage: true,
+    // paginationSize: 5,
+    hidePageListOnlyOnePage: true,
+    // clearSearch: true,
+    alwaysShowAllBtns: false,
+    onRowClick: HandlerowSelect,
+    withFirstAndLast: false,
+
+    // onPageChange: onPageChange,
+    // onSizePerPageList: sizePerPageListChange
+  };
+
+  //---- Material Table
+
+  // const columns = [
+  //   {
+  //     name: "FuelcostId",
+  //     label: "ID",
+  //     options: {
+  //       filter: false,
+  //       sort: false,
+  //       display: false
+  //     }
+  //   },
+  //   {
+  //     name: "StartDate",
+  //     label: "From",
+  //     options: {
+  //       filter: true,
+  //       sort: true
+  //     }
+  //   },
+  //   {
+  //     name: "EndDate",
+  //     label: "To",
+  //     options: {
+  //       filter: true,
+  //       sort: true
+  //     }
+  //   },
+  //   {
+  //     name: "CostPerLitre",
+  //     label: "Fuel Cost",
+  //     options: {
+  //       filter: true,
+  //       sort: true
+  //     }
+  //   },
+  //   {
+  //     name: "CurrencyCode",
+  //     label: "Currency",
+  //     options: {
+  //       filter: true,
+  //       sort: true
+  //     }
+  //   },
+  //   {
+  //     name: "active",
+  //     label: "Status",
+  //     options: {
+  //       filter: false,
+  //       sort: false,
+  //       display: false
+  //     }
+  //   }
+  // ];
+
+  // const options = {
+  //   filterType: "multiselect",
+  //   onRowClick: (rowData, rowMeta) => HandlerowSelect(rowData, rowMeta),
+  //   rowsPerPageOptions: [2, 5, 10, 15, 20, 100],
+  //   selectableRows: "none",
+  //   viewColumns: true
+
+  //   // onRowsSelect: (currentRowsSelected, allRowsSelected) => console.log(currentRowsSelected, ' : ', allRowsSelected ),
+  // };
+
+  let Tabledisplay = (
+    <LinearProgress style={classes.linearprogress} color="secondary" />
+  );
+  let PageSizeComp = (
+    <select onChange={handlePageSize} value={PageSize}>
+      <option selected />
+      <option value={10}>10</option>
+      <option value={20}>20</option>
+    </select>
+  );
+  let [Tabledistatus, settabledistatus] = useState(false);
+
+
+  if (Tabledistatus) {
+    Tabledisplay = (
+      // <MUIDataTable
+      //   title={"Actions & Filters"}
+      //   data={Atlist}
+      //   columns={columns}
+      //   options={options}
+      // />
+      <div>
+        <BootstrapTable
+          data={Atlist}
+          version="4"
+          striped
+          hover
+          pagination
+          search
+          options={options}
+        >
+          <TableHeaderColumn dataField="registration" dataSort>
+            Registration
+          </TableHeaderColumn>
+          <TableHeaderColumn isKey dataField="vehicleGroupId" dataSort>
+            Group
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField="refernce" dataSort>
+            Reference
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField="vehicleTypeId" dataSort>
+            Vehicle Type
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField="costPerMile" dataSort>
+            Cost Per Mile
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField="resourceId" dataSort>
+            Fixed Resource
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField="trackingDeviceId" dataSort>
+            Tracking Device
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField="usedForJobs" dataSort>
+            Used For Jobs
+          </TableHeaderColumn>
+        </BootstrapTable>
+
+        <br />
+        <div className="row">
+          <div className="col">
+            {PageSizeComp}
+            {"  Showing " + PageSize + " Rows Per Page"}
+          </div>
+          <div className="col">{paging}</div>
+        </div>
+      </div>
+    );
+  } else {
+    Tabledisplay = (
+      <LinearProgress style={classes.linearprogress} color="secondary" />
+    );
+  }
+
+  let icon=(status)=>{
+    let iconvalue = <Icon2 />
+    if(status===true){
+        iconvalue = <DoneOutlineIcon />
+    }
+    else if(status===false){
+        iconvalue = <Icon2 />
+    }
+    return iconvalue;
+}
+
+let iconint=(status)=>{
+  let iconvalue = <Icon2 />
+  if(status!==''){
+      iconvalue = <DoneOutlineIcon />
+  }
+  else{
+      iconvalue = <Icon2 />
+  }
+  return iconvalue;
+}
+
+
+  let refreshfn = () => {
+    // settabledistatus((Tabledistatus = false));
+    getlistapi();
+  };
+
+  useEffect(() => {
+    getlistapi();
+  }, []);
+
+  async function getlistapi() {
+    await GetListingForVehiclemanage(Page, PageSize).then(res => {
+      setAtlist((Atlist = res.data));
+      setPaginate((paginate = JSON.parse(res.headers["x-pagination"])));
+    });
+    Atlist.map(
+      (e, i) =>
+        (
+        Atlist[i].trackingDeviceId =iconint(Atlist[i].trackingDeviceId),
+        Atlist[i].usedForJobs =icon(Atlist[i].usedForJobs)
+        )
+    );
+    TotalPages = paginate.totalPages;
+    // Atlist.map((e,i)=>
+    //   Atlist[i].action=<i className="icon-options icons font-2xl d-block mt-4" ></i>
+
+    //                 )
+    settabledistatus((Tabledistatus = false));
+    settabledistatus((Tabledistatus = true));
+  }
+
+  // Toast
+
+  function errort() {
+    // add type: 'error' to options
+    return toast.error("Failed with Error...", {
+      position: toast.POSITION.BOTTOM_RIGHT
+    });
+  }
+  function success() {
+    return toast.success("Deleted Successfully... ", {
+      position: toast.POSITION.BOTTOM_RIGHT
+    });
+  }
+
+  //--- Pagination ------------------
+
+  let [pgin, setPgin] = useState(true);
+
+  function handlepagin() {
+    setPgin(false);
+    // setTimeout(() => setPgin(true), 10);
+    refreshfn();
+    setPgin(true);
+  }
+
+  if (pgin) {
+    if (Page > 2 || Page === 2) {
+      if (Page === TotalPages) {
+        paging = (
+          <Pagination>
+            <PaginationItem>
+              <PaginationLink
+                previous
+                tag="button"
+                onClick={() => {
+                  Page = Page - 1;
+                  handlepagin();
+                }}
+              />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink
+                tag="button"
+                onClick={() => {
+                  Page = Page - 2;
+                  handlepagin();
+                }}
+              >
+                {Page - 2}
+              </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink
+                tag="button"
+                onClick={() => {
+                  Page = Page - 1;
+                  handlepagin();
+                }}
+              >
+                {Page - 1}
+              </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink
+                tag="button"
+                // onClick={() => {
+                //   Page = Page+1;
+                //   handlepagin();
+
+                // }}
+              >
+                {Page}
+              </PaginationLink>
+            </PaginationItem>
+          </Pagination>
+        );
+      } else if (Page === TotalPages - 1) {
+        paging = (
+          <Pagination>
+            <PaginationItem>
+              <PaginationLink
+                previous
+                tag="button"
+                onClick={() => {
+                  Page = Page - 1;
+                  handlepagin();
+                }}
+              />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink
+                tag="button"
+                onClick={() => {
+                  Page = Page - 2;
+                  handlepagin();
+                }}
+              >
+                {Page - 2}
+              </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink
+                tag="button"
+                onClick={() => {
+                  Page = Page - 1;
+                  handlepagin();
+                }}
+              >
+                {Page - 1}
+              </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink
+                tag="button"
+                // onClick={() => {
+                //   Page = Page+1;
+                //   handlepagin();
+
+                // }}
+              >
+                {Page}
+              </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink
+                tag="button"
+                onClick={() => {
+                  Page = Page + 1;
+                  handlepagin();
+                }}
+              >
+                {Page + 1}
+              </PaginationLink>
+            </PaginationItem>
+          </Pagination>
+        );
+      } else {
+        paging = (
+          <Pagination>
+            <PaginationItem>
+              <PaginationLink
+                previous
+                tag="button"
+                onClick={() => {
+                  Page = Page - 1;
+                  handlepagin();
+                }}
+              />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink
+                tag="button"
+                onClick={() => {
+                  Page = Page - 1;
+                  handlepagin();
+                }}
+              >
+                {Page - 1}
+              </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink
+                tag="button"
+                onClick={() => {
+                  Page = Page;
+                  handlepagin();
+                }}
+              >
+                {Page}
+              </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink
+                tag="button"
+                onClick={() => {
+                  Page = Page + 1;
+                  handlepagin();
+                }}
+              >
+                {Page + 1}
+              </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink
+                tag="button"
+                onClick={() => {
+                  Page = Page + 2;
+                  handlepagin();
+                }}
+              >
+                {Page + 2}
+              </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink
+                next
+                tag="button"
+                onClick={() => {
+                  Page = Page + 1;
+                  handlepagin();
+                }}
+              />
+            </PaginationItem>
+          </Pagination>
+        );
+      }
+    } else if (Page < 2) {
+      paging = (
+        <Pagination>
+          <PaginationItem>
+            <PaginationLink
+              tag="button"
+              onClick={() => {
+                Page = Page;
+                handlepagin();
+              }}
+            >
+              {Page}
+            </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink
+              tag="button"
+              onClick={() => {
+                Page = Page + 1;
+                handlepagin();
+              }}
+            >
+              {Page + 1}
+            </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink
+              tag="button"
+              onClick={() => {
+                Page = Page + 2;
+                handlepagin();
+              }}
+            >
+              {Page + 2}
+            </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink
+              next
+              tag="button"
+              onClick={() => {
+                Page = Page + 1;
+                handlepagin();
+              }}
+            />
+          </PaginationItem>
+        </Pagination>
+      );
+    }
+  } else {
+    paging = "";
+  }
+  function handlePageSize(event) {
+    PageSize = event.target.value;
+    refreshfn();
+  }
+
+
+
+  //----- Finished Pagination---------
+
+  async function Dellistapi() {
+    await DeleteVehiclemanageDataById(idofEdit)
+      .then(() => {
+        success();
+      })
+      .catch(error => {
+        errort();
+      });
+    Handlerowclose();
+    refreshfn();
+  }
+
+  let [Editstate, setEditstate] = React.useState(false);
+  let HandleEditforlisting = () => {
+    return (
+      setEditstate((Editstate = !Editstate)),
+      Handlerowclose()
+      // handleMenuClose()
+    );
+  };
+
+  let HandleCrossEditforlisting = () => {
+    return setEditstate((Editstate = false));
+  };
+
+  if (Editstate) {
+    EditshowModel = (
+      <EditFuelCost
+        IDforAPI={idofEdit}
+        refresh={refreshfn}
+        cross={HandleCrossEditforlisting}
+      />
+    );
+  } else {
+    EditshowModel = "";
+  }
+
+  let [menushow, setMenushow] = useState(false);
+  function HandlerowSelect  (row) {
+    menuDiv = "";
+    idofEdit = row.fuelcostId;
+    return setMenushow((menushow = true));
+  };
+  let Handlerowclose = (row) => {
+    return setMenushow((menushow = false));
+  };
+  if (menushow) {
+    menuDiv = (
+      <ul className="tool">
+        <li>
+          <VehicleFuelCostAdd refresh={refreshfn} />
+        </li>
+        <li onClick={HandleEditforlisting}>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <i className="fa fa-pencil-square fa-2x" />
+        </li>
+        <li onClick={Dellistapi}>
+          &nbsp;&nbsp;
+          <i className="fa fa-archive fa-2x" />
+        </li>
+        <li onClick={Handlerowclose}>
+          &nbsp;&nbsp;
+          <i className="fa fa-times-rectangle fa-2x" />
+        </li>
+      </ul>
+    );
+  } else if (!menushow) {
+    menuDiv = (
+      <ul className="tool">
+        <li />
+        <VehicleFuelCostAdd refresh={refreshfn} />
+      </ul>
+    );
+  }
+
+  return (
+    <div>
+      <div className="row header">
+        <div className="col-12 col-sm-6 col-md-5 col-lg-5 col-xl-5">
+          {menuDiv}
+        </div>
+        <div className="col-12 col-sm-6 col-md-7 col-lg-7 col-xl-7">
+          <h3 className="heading">MANAGE VEHICLES</h3>
+        </div>
+      </div>
+      <br />
+      {EditshowModel}
+      {Tabledisplay}
+    </div>
+  );
+
 }
 
 export default VehicleManage;
---
+++ src/components/myaccount/admin/vehicle/shared/manage.js
@@ -0,0 +1,53 @@
+import { apiUrl } from "..//..//..//..//..//config.json";
+import axios from "axios";
+
+/* *************************VVehicle Fuel Cost*********************** */
+let apiurlVehiclemanage = apiUrl + "/masterdata/Vehicles";
+// let apiurlVehiclemanage= 'https://reqres.in/api/users/2';
+
+function ApiwithidVehiclemanage(Id) {
+  return `${apiurlVehiclemanage}/${Id}`;
+}
+
+function ApibyPageVehiclemanage(Pg, PgSize) {
+  return `${apiurlVehiclemanage}/${Pg}/${PgSize}`;
+}
+
+//-------- GetAPi
+export function GetListingForVehiclemanage(pg, pgsize) {
+  return axios.get(ApibyPageVehiclemanage(pg, pgsize), {
+    headers: {
+      //"Authorization": authIt,
+      //"accept": "application/json",
+      //"Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json"
+    }
+  });
+}
+
+//---------Edit APi
+export function GetVehiclemanageDataById(id) {
+  // console.log(id)
+  return axios.get(ApiwithidVehiclemanage(id), {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+}
+
+export function PutVehiclemanageDataById(id, body) {
+  // console.log(id)
+  return axios.put(ApiwithidVehiclemanage(id), body);
+}
+
+//------- Delete-API
+
+export function DeleteVehiclemanageDataById(id) {
+  // console.log(id)
+  return axios.delete(ApiwithidVehiclemanage(id));
+}
+
+//----------Save-Post-API
+export function PostListingForVehiclemanage(body) {
+  return axios.post(apiurlVehiclemanage, body);
+}
