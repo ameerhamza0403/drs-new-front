20e5120a975a4e0f87c9ee29b34fbac107316f27
ameerhamza0403 <ah04031996@gmail.com>
2019-08-23 17:39:39 +0500

    hamza

--- src/components/myaccount/admin/vehicle/fuelcheck/edit.js
+++ src/components/myaccount/admin/vehicle/fuelcheck/edit.js
@@ -20,6 +20,8 @@ import { Formik } from "formik";
 import * as Yup from "yup";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import TextField from "@material-ui/core/TextField";
+import { GetListingForcurrency } from "../../resources/shared/currency";
 
 const classes = {
   button: {
@@ -35,40 +37,79 @@ const classes = {
     float: "left"
     // marginTop: '10px',
     // marginLeft: '5px',
+  },
+  input: {
+    width: "100px",
+    // height: "25px",
+    border: "1px solid black",
+    textAlign: "center"
   }
 };
 
 let EditFuelCost = props => {
   // getModalStyle is not a pure function, we roll the style only on the first render
 
-    //Toast
-
-    function errort() {
-      // add type: 'error' to options
-      return toast.error('Failed with Error...', {
-        position: toast.POSITION.BOTTOM_RIGHT
-      });
-
-    }
-    function success() {
-      return toast.success("Saved Successfully... ", {
-        position: toast.POSITION.BOTTOM_RIGHT
-      });
-    }
-
-
+  let [timevalue,setTimevalue] = useState({
+    startDate: '',
+    endDate: '',
+  });
   async function onSubmit(values, { setSubmitting, setErrors }) {
-    await PutVehiclefuelcostDataById(props.IDforAPI, values).then(()=>success()).catch(error=>errort());
+    let newvalue=values;
+    newvalue.startDate=timevalue.startDate;
+    newvalue.endDate=timevalue.endDate;
+    await PutVehiclefuelcostDataById(props.IDforAPI, newvalue).then(()=>success()).catch(error=>errort());
     handleOpen();
     props.refresh();
     setSubmitting(false);
   }
 
+  let [currency, setCurrency] = useState([
+    {
+      currencyId: "",
+      name: "",
+      code: "",
+      active: true
+    }
+  ]);
+  useEffect(() => {
+    getcurrlist();
+    getlistapi();
+  }, []);
+
+  async function getcurrlist() {
+    const { data: currency } = await GetListingForcurrency();
+    setCurrency(currency);
+  }
+
+  async function getlistapi() {
+    const { data: initialValues } = await GetVehiclefuelcostDataById(
+      props.IDforAPI
+    );
+    setInitialValues(initialValues);
+  }
+
+  //Tost
+
+  function errort() {
+    // add type: 'error' to options
+    return toast.error("Failed with Error...", {
+      position: toast.POSITION.BOTTOM_RIGHT
+    });
+  }
+  function success() {
+    return toast.success("Saved Successfully... ", {
+      position: toast.POSITION.BOTTOM_RIGHT
+    });
+  }
+
   const validationSchema = function(values) {
     return Yup.object().shape({
-      name: Yup.string()
-        .min(4, `Vehicle Type has to be at least 4 characters`)
-        .required("Vehicle Type is required")
+      costPerLitre: Yup.string()
+        .min(1, `Fuel Cost has to be at least 1 characters`)
+        .required("Fuel Cost is required"),
+      currencyId: Yup.string()
+        // .min(1, `Fuel Cost has to be at least 1 characters`)
+        .required("Currency is required")
     });
   };
 
@@ -95,8 +136,7 @@ let EditFuelCost = props => {
   };
 
   const [initialValues, setInitialValues] = useState({
-    name: "",
-    active: false
+
   });
 
   function findFirstError(formName, hasError) {
@@ -121,25 +161,27 @@ let EditFuelCost = props => {
     });
     validateForm(errors);
   }
+
   let [modal, setModal] = useState(true);
 
   let handleOpen = () => {
     return setModal((modal = false)), setTimeout(() => props.cross(), 200);
   };
 
-  useEffect(() => {
-    getlistapi();
-  }, []);
 
-  async function getlistapi() {
-    const { data: initialValues } = await GetVehiclefuelcostDataById(
-      props.IDforAPI
-    );
-    setInitialValues(initialValues);
-  }
-
+  const handleDataChange = name => event => {
+    if (name === "startDate") {
+      setTimevalue( {...timevalue, [name] : event.target.value});
+    } else {
+      setTimevalue( {...timevalue, [name] : event.target.value});
+    }
+  };
   return (
     <div>
+      <div onClick={handleOpen} style={classes.plusbutton}>
+        <i className="fa fa-plus-circle fa-2x" />
+      </div>
+
       <Modal
         isOpen={modal}
         toggle={handleOpen}
@@ -175,41 +217,117 @@ let EditFuelCost = props => {
                             <Label for="name">Fuel Cost</Label>
                           </div>
                           <div className="col-12 col-sm-12 col-md-6 col-lg-8 col-xl-8">
-                            <Input
-                              type="text"
-                              name="name"
-                              id="name"
-                              placeholder={initialValues.name}
-                              autoComplete="given-name"
-                              valid={!errors.name}
-                              invalid={touched.name && !!errors.name}
-                              autoFocus={true}
-                              required
-                              onChange={handleChange}
-                              onBlur={handleBlur}
-                              value={values.name}
-                            />
-                            <FormFeedback>{errors.name}</FormFeedback>
-                            <br />
-                            <input
-                              name="active"
-                              id="active"
-                              valid={!errors.active}
-                              invalid={touched.active && !!errors.active}
-                              onClick={handleChange}
-                              onBlur={handleBlur}
-                              value={values.active}
-                              type="checkbox"
-                            />
-                            &nbsp;&nbsp;&nbsp;
-                            <label
-                              className="form-check-label"
-                              for="defaultCheck1"
-                            >
-                              Active
-                            </label>
+                            <div className="row">
+                              <div className="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+                                <Input
+                                  type="text"
+                                  name="costPerLitre"
+                                  id="name"
+                                  placeholder={values.costPerLitre}
+                                  autoComplete="given-name"
+                                  valid={!errors.costPerLitre}
+                                  invalid={
+                                    touched.costPerLitre &&
+                                    !!errors.costPerLitre
+                                  }
+                                  autoFocus={true}
+                                  required
+                                  onChange={handleChange}
+                                  onBlur={handleBlur}
+                                  // value={values.costPerLitre}
+                                  maxLength={8}
+                                  style={classes.input}
+                                />
+                                <FormFeedback>
+                                  {errors.costPerLitre}
+                                </FormFeedback>
+                              </div>
+                              <div className="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+                                <Input
+                                  type="select"
+                                  name="currencyId"
+                                  id="currencyId"
+                                  autoComplete="given-name"
+                                  valid={!errors.currencyId}
+                                  invalid={
+                                    touched.currencyId && !!errors.currencyId
+                                  }
+                                  autoFocus={true}
+                                  required
+                                  onChange={handleChange}
+                                  onBlur={handleBlur}
+                                  value={values.currencyId}
+                                >
+                                  <option value="" />
+                                  {currency.map(e => (
+                                    <option value={e.currencyId}>
+                                      {e.code}
+                                    </option>
+                                  ))}
+                                </Input>
+                                <FormFeedback>{errors.currencyId}</FormFeedback>
+                                <br />
+                              </div>
+                            </div>
                           </div>
-                        </div>
+                          </div>
+                          <br />
+                          <div className="row">
+                            <div className="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
+                              <Label for="name">From</Label>
+                            </div>
+                            <div className="col-12 col-sm-12 col-md-6 col-lg-8 col-xl-8">
+                              <TextField
+                                id="date"
+                                label="startDate"
+                                type="date"
+                                defaultValue={timevalue.startDate}
+                                InputLabelProps={{
+                                  shrink: true
+                                }}
+                                onChange={handleDataChange("startDate")}
+                              />
+                            </div>
+                          </div>
+                          <br />
+                          <div className="row">
+                            <div className="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
+                              <Label for="name">To</Label>
+                            </div>
+                            <div className="col-12 col-sm-12 col-md-6 col-lg-8 col-xl-8">
+                              <TextField
+                                id="date"
+                                label="endDate"
+                                type="date"
+                                defaultValue={timevalue.endDate}
+                                InputLabelProps={{
+                                  shrink: true
+                                }}
+                                onChange={handleDataChange("endDate")}
+                              />
+                            </div>
+                          </div>
+                          <br />
+                          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                          &nbsp;&nbsp;&nbsp;&nbsp;
+                          <input
+                            name="active"
+                            id="active"
+                            valid={!errors.active}
+                            invalid={touched.active && !!errors.active}
+                            onClick={handleChange}
+                            onBlur={handleBlur}
+                            value={values.active}
+                            type="checkbox"
+                          />
+                          &nbsp;&nbsp;&nbsp;
+                          <label
+                            className="form-check-label"
+                            for="defaultCheck1"
+                          >
+                            Active
+                          </label>
                       </FormGroup>
                       <FormGroup>
                         <ModalFooter>
@@ -243,5 +361,6 @@ let EditFuelCost = props => {
     </div>
   );
 };
+
 
 export default EditFuelCost;
