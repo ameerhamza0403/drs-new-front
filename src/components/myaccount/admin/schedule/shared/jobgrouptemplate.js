609702e18b0ae0ec7f3b4ca788e9ee51b29e3f17
ameerhamza0403 <ah04031996@gmail.com>
2019-08-26 19:29:11 +0500

    hamza

--- src/components/myaccount/admin/schedule/shared/jobgrouptemplate.js
+++ src/components/myaccount/admin/schedule/shared/jobgrouptemplate.js
@@ -6,6 +6,9 @@ import axios from 'axios';
 /* *************************Job Group Template*********************** */
 let apiurljobgrouptemplate= apiUrl + '/masterdata/JobGroupTemplates';
 // let apiurlAbsenceType= 'https://reqres.in/api/users/2';
+let apiurljobgrouptype= apiUrl + '/masterdata/JobTypes/0/0';
+
+
 
 function Apiwithidjobgrouptemplate(Id) {
     return `${apiurljobgrouptemplate}/${Id}`;
@@ -57,5 +60,74 @@ export function DeletejobgrouptemplateDataById(id) {
 //----------Save-Post-API
 export function PostListingForjobgrouptemplate(body) {
     return axios.post(apiurljobgrouptemplate,body)
+
+}
+
+
+//------------------ Group type -------
+export function JobGroupType(){
+  return axios.get(apiurljobgrouptype,
+    {headers: {
+        //"Authorization": authIt,
+        //"accept": "application/json",
+        //"Access-Control-Allow-Origin": "*",
+        'Content-Type': 'application/json',
+        'Access-Control-Expose-Headers': '*',
+       }});
+}
+
+
+//-------------- Group Template -------- /masterdata/JobGroupTemplates
+
+/* *************************Job Group Template*********************** */
+function apiurltemplatejob(Id){
+  return `${apiurljobgrouptemplate}/${Id}/TemplateJobs`;
+}
+// let apiurlAbsenceType= 'https://reqres.in/api/users/2';
+
+
+
+// //-------- GetAPi
+// export function GetListingForjobgrouptemplate(pg,pgsize) {
+//     return axios.get(ApiwithbyPgjobgrouptemplate(pg,pgsize),
+//              {headers: {
+//                  //"Authorization": authIt,
+//                  //"accept": "application/json",
+//                  //"Access-Control-Allow-Origin": "*",
+//                  'Content-Type': 'application/json',
+//                  'Access-Control-Expose-Headers': '*',
+//                 }}
+//             )
+
+// }
+
+// //---------Edit APi
+// export function GetjobgrouptemplateDataById(id) {
+//     // console.log(id)
+//     return axios.get(Apiwithidjobgrouptemplate(id),
+//              {headers: {
+//                  'Content-Type': 'application/json',
+//                 }}
+//             )
+
+// }
+
+// export function PutjobgrouptemplateDataById(id,body) {
+//     // console.log(id)
+//     return axios.put(Apiwithidjobgrouptemplate(id),body)
+
+// }
+
+// //------- Delete-API
+
+// export function DeletejobgrouptemplateDataById(id) {
+//     // console.log(id)
+//     return axios.delete(Apiwithidjobgrouptemplate(id))
+
+// }
+
+//----------Save-Post-API
+export function PostListingFortemplategroup(id,body) {
+    return axios.post(apiurltemplatejob(id),body)
 
 }
